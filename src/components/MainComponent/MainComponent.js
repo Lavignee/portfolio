e85794015e9fa66414f0996a5374234e620ea5d3
Lavignee <ldy9243@gmail.com>
2020-10-31 08:45:14 +0900

    1. 출력은 비율로, 애니메이션 계산값은 고정px로 처리

--- src/components/MainComponent/MainComponent.js
+++ src/components/MainComponent/MainComponent.js
@@ -17,113 +17,34 @@ const MainComponent = () => {
   }));
 
   useEffect(() => {
-    gsap.to('.target1', {
-      // scale: 0.8,
-      x: '100%',
-      y: -400,
-      scrollTrigger: {
-        trigger: '.main-text-frame',
-        start: 'top',
-        end: 'bottom',
-        scrub: true,
-        markers: true
-      }
+    const canvasFrames = gsap.utils.toArray('.canvas-frame');
+    const targetToLefts = gsap.utils.toArray('.left');
+    const targetToRights = gsap.utils.toArray('.right');
+    const scrollTriggers = {
+      trigger: '.main-text-frame',
+      start: 'top',
+      end: 'bottom',
+      scrub: true,
+    }
+    canvasFrames.forEach(target => {
+      gsap.to(target, {
+        autoAlpha: 0.1,
+        scale: 0.8,
+        y: -300,
+        scrollTrigger: scrollTriggers
+      })
     });
-
-    ScrollTrigger.matchMedia({
-      // "(min-width:1025px)": function() {
-      //   sections.forEach(section => {
-      //     const img = section.querySelector('img');
-      //     ScrollTrigger.create({
-      //       trigger: img,
-      //       start: 'top top+=150',
-      //       pin: true
-      //     })
-      //   })
-      // }
-      // "(min-width:1025px)": function () {
-      //   gsap.to('.target1', {
-      //     // scale: 0.8,
-      //     x: 50,
-      //     y: -400,
-      //     scrollTrigger: {
-      //       trigger: '.main-text-frame',
-      //       start: 'top',
-      //       end: 'bottom',
-      //       scrub: true,
-      //       markers: true
-      //     }
-      //   });
-      // },
-
-      // "all": function () {
-      //   gsap.to('.target1', {
-      //     // scale: 0.8,
-      //     x: 50,
-      //     y: -400,
-      //     scrollTrigger: {
-      //       trigger: '.main-text-frame',
-      //       start: 'top',
-      //       end: 'bottom',
-      //       scrub: true,
-      //       markers: true
-      //     }
-      //   });
-      // }
+    targetToLefts.forEach(target => {
+      gsap.to(target, {
+        x: -50,
+        scrollTrigger: scrollTriggers
+      });
     });
-    gsap.to('.target2', {
-      // scale: 0.8,
-      x: 29,
-      y: -400,
-      scrollTrigger: {
-        trigger: '.main-text-frame',
-        start: 'top',
-        end: 'bottom',
-        scrub: true,
-      }
-    });
-    gsap.to('.target3', {
-      // scale: 0.8,
-      x: -20,
-      y: -400,
-      scrollTrigger: {
-        trigger: '.main-text-frame',
-        start: 'top',
-        end: 'bottom',
-        scrub: true,
-      }
-    });
-    gsap.to('.target4', {
-      // scale: 0.8,
-      x: -30,
-      y: -400,
-      scrollTrigger: {
-        trigger: '.main-text-frame',
-        start: 'top',
-        end: 'bottom',
-        scrub: true,
-      }
-    });
-    gsap.to('.target5', {
-      // scale: 0.8,
-      x: 15,
-      y: -400,
-      scrollTrigger: {
-        trigger: '.main-text-frame',
-        start: 'top',
-        end: 'bottom',
-        scrub: true,
-      }
-    });
-    gsap.to('.target6', {
-      // scale: 0.8,
-      y: -400,
-      scrollTrigger: {
-        trigger: '.main-text-frame',
-        start: 'top',
-        end: 'bottom',
-        scrub: true,
-      }
+    targetToRights.forEach(target => {
+      gsap.to(target, {
+        x: 50,
+        scrollTrigger: scrollTriggers
+      });
     });
   }, []);
 
@@ -131,12 +52,12 @@ const MainComponent = () => {
     <section className='container main'>
       {/* TODO: Hooks로 코드 더 간결하게 작성해보자. */}
       {/* <VideoToCanvasComponent src={src} /> */}
-      <CanvasVideo src={src} target={'target1'} maskX={-10} maskY={0} resolX={1280} resolY={720} sizeX={500} sizeY={250} />
-      <CanvasVideo src={src} target={'target2'} maskX={0} maskY={-200} resolX={1280} resolY={720} sizeX={800} sizeY={400} />
-      <CanvasVideo src={src} target={'target3'} maskX={-200} maskY={-150} resolX={1280} resolY={720} sizeX={600} sizeY={300} />
-      <CanvasVideo src={src} target={'target4'} maskX={-350} maskY={-100} resolX={1280} resolY={720} sizeX={700} sizeY={350} />
-      <CanvasVideo src={src} target={'target5'} maskX={0} maskY={-100} resolX={1280} resolY={720} sizeX={600} sizeY={300} />
-      <CanvasVideo src={src} target={'target6'} maskX={0} maskY={0} resolX={1280} resolY={720} sizeX={300} sizeY={150} />
+      <CanvasVideo src={src} target={'targets target1 right'} maskX={-10} maskY={0} resolX={1280} resolY={720} sizeX={500} sizeY={250} />
+      <CanvasVideo src={src} target={'targets target2 right'} maskX={0} maskY={-200} resolX={1280} resolY={720} sizeX={800} sizeY={400} />
+      <CanvasVideo src={src} target={'targets target3 left'} maskX={-200} maskY={-150} resolX={1280} resolY={720} sizeX={600} sizeY={300} />
+      <CanvasVideo src={src} target={'targets target4 left'} maskX={-350} maskY={-100} resolX={1280} resolY={720} sizeX={700} sizeY={350} />
+      <CanvasVideo src={src} target={'targets target5 right'} maskX={0} maskY={-100} resolX={1280} resolY={720} sizeX={600} sizeY={300} />
+      <CanvasVideo src={src} target={'targets target6 right'} maskX={0} maskY={0} resolX={1280} resolY={720} sizeX={300} sizeY={150} />
       <div className='main-text-frame'>
         <div className='main-text'>
           <span>FRONT - END</span><span>DEVELOPER</span>
