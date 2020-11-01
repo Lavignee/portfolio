8f58384f7aeb28c79589545dabbf12aa2a39de5a
Lavignee <ldy9243@gmail.com>
2020-11-01 11:35:36 +0900

    디바이스별로 다른 영상 출력 고려.

--- src/components/MainComponent/MainComponent.js
+++ src/components/MainComponent/MainComponent.js
@@ -1,16 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Trans, withTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { gsap } from "gsap";
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import CanvasVideo from '../../lib/CanvasVideo'
-import src from '../../Static/videos/video.mp4'
+import src from '../../Static/videos/video1280.mp4'
+// import srcSmall from '../../Static/videos/video640.mp4'
 import './MainComponent.scss';
 // import VideoToCanvasComponent from '../VideoToCanvasComponent'
 // import TypingAnimationComponent from '../TypingAnimationComponent';
 
 gsap.registerPlugin(ScrollTrigger);
 
+const CallTheVideo = (VideoSource, resolX, resolY) => {
+  return (
+    <>
+      <CanvasVideo src={VideoSource} target={'targets target1 right'} maskX={-10} maskY={0} resolX={resolX} resolY={resolY} sizeX={500} sizeY={250} />
+      <CanvasVideo src={VideoSource} target={'targets target2 right'} maskX={0} maskY={-200} resolX={resolX} resolY={resolY} sizeX={800} sizeY={400} />
+      <CanvasVideo src={VideoSource} target={'targets target3 left'} maskX={-200} maskY={-150} resolX={resolX} resolY={resolY} sizeX={600} sizeY={300} />
+      <CanvasVideo src={VideoSource} target={'targets target4 left'} maskX={-350} maskY={-100} resolX={resolX} resolY={resolY} sizeX={700} sizeY={350} />
+      <CanvasVideo src={VideoSource} target={'targets target5 right'} maskX={0} maskY={-100} resolX={resolX} resolY={resolY} sizeX={600} sizeY={300} />
+      <CanvasVideo src={VideoSource} target={'targets target6 right'} maskX={0} maskY={0} resolX={resolX} resolY={resolY} sizeX={300} sizeY={150} />
+    </>
+  )
+}
 const MainComponent = () => {
   const { language } = useSelector(state => ({
     language: state.LanguageModule.language
@@ -28,7 +41,6 @@ const MainComponent = () => {
     }
     canvasFrames.forEach(target => {
       gsap.to(target, {
-        autoAlpha: 0.1,
         scale: 0.8,
         y: -300,
         scrollTrigger: scrollTriggers
@@ -52,12 +64,12 @@ const MainComponent = () => {
     <section className='container main'>
       {/* TODO: Hooks로 코드 더 간결하게 작성해보자. */}
       {/* <VideoToCanvasComponent src={src} /> */}
-      <CanvasVideo src={src} target={'targets target1 right'} maskX={-10} maskY={0} resolX={1280} resolY={720} sizeX={500} sizeY={250} />
-      <CanvasVideo src={src} target={'targets target2 right'} maskX={0} maskY={-200} resolX={1280} resolY={720} sizeX={800} sizeY={400} />
-      <CanvasVideo src={src} target={'targets target3 left'} maskX={-200} maskY={-150} resolX={1280} resolY={720} sizeX={600} sizeY={300} />
-      <CanvasVideo src={src} target={'targets target4 left'} maskX={-350} maskY={-100} resolX={1280} resolY={720} sizeX={700} sizeY={350} />
-      <CanvasVideo src={src} target={'targets target5 right'} maskX={0} maskY={-100} resolX={1280} resolY={720} sizeX={600} sizeY={300} />
-      <CanvasVideo src={src} target={'targets target6 right'} maskX={0} maskY={0} resolX={1280} resolY={720} sizeX={300} sizeY={150} />
+      { matchMedia("screen and (min-width: 985px)").matches ? (
+        CallTheVideo(src, 1280, 720)
+      ) : (
+          CallTheVideo(src, 1280, 720)
+          // CallTheVideo(srcSmall, 640, 480)
+        )}
       <div className='main-text-frame'>
         <div className='main-text'>
           <span>FRONT - END</span><span>DEVELOPER</span>
