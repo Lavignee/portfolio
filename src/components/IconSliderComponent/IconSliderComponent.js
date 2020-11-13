e5d813a58a88c4c3ad0970b76ac79dbca5b59c9e
Lavignee <ldy9243@gmail.com>
2020-11-13 15:53:32 +0900

    아이콘슬라이더 반응형대응 준비

--- src/components/IconSliderComponent/IconSliderComponent.js
+++ src/components/IconSliderComponent/IconSliderComponent.js
@@ -1,29 +1,29 @@
 import React, { useEffect } from 'react';
 import { gsap } from 'gsap';
-import html5 from '../../static/images/001_HTML5.svg';
-import css from '../../static/images/002_css.svg';
-import javasc from '../../static/images/003_javasc.svg';
-import react from '../../static/images/004_react.svg';
-import webpack from '../../static/images/005_webpack.svg';
-import parcel from '../../static/images/006_parcel.svg';
-import sass from '../../static/images/007_sass.svg';
-import greens from '../../static/images/008_greens.svg';
-import github from '../../static/images/014_github.svg';
+import useWindowSize from "../../utils/useWindowSize";
+import svg from '../../static/images/icon-svg.json';
 import './IconSliderComponent.scss';
 
 const IconsliderComponent = ({ reverse }) => {
-  const colors = ["#000000", "#111111", "#222222", "#333333", "#444444", "#555555", "#666666", "#777777", "#888888", "#999999", "#aaaaaa"];
-  const icons = [html5, css, javasc, react, webpack, parcel, sass, greens, github]
+  const { width, height } = useWindowSize();
+
+  // const colors = ["#000000", "#111111", "#222222", "#333333", "#444444", "#555555", "#666666", "#777777", "#888888", "#999999", "#aaaaaa"];
+  const allIcons = [svg.bitbucket, svg.bootstrap, svg.css, svg.figma, svg.gatsby, svg.git, svg.github, svg.graphql, svg.gsap, svg.html5, svg.i18next, svg.javascript, svg.jira, svg.jquery, svg.materialui, svg.mobx, svg.netlify, svg.nextjs, svg.parcel, svg.react, svg.redux, svg.sass, svg.sourcetree, svg.typescript, svg.webgl, svg.webpack, svg.zeplin]
+  const wellIcons = [svg.bitbucket, svg.bootstrap, svg.css, svg.figma, svg.git, svg.github, svg.gsap, svg.html5, svg.i18next, svg.javascript]
+  const wellIcons2 = [svg.jira, svg.jquery, svg.materialui, svg.parcel, svg.react, svg.redux, svg.sass, svg.sourcetree, svg.webpack, svg.zeplin]
+  const bedIcons = [svg.gatsby, svg.graphql, svg.mobx, svg.netlify, svg.nextjs, svg.typescript, svg.webgl]
 
   useEffect(() => {
+    console.log(width)
+    console.log(height)
+  }, [width, height])
+
+  useEffect(() => {
+    // console.log(bedIcons.slice(0, 5))
     gsap.set(".content-frame", {
       // backgroundColor: (i) => colors[i % colors.length],
       x: (i) => i * 100 + '%'
     });
-
-    // gsap.set(".content", {
-    //   backgroundImage: (i) => icons[i % icons.length],
-    // });
 
     gsap.to(".content-frame", {
       duration: 100,
@@ -38,11 +38,17 @@ const IconsliderComponent = ({ reverse }) => {
 
   return (
     <div className={`icon-slider${reverse ? ` ${reverse}` : ''}`}>
-      {icons.map(icons => (
-        <div key={icons} className='content-frame'><div className='content'><img src={icons} alt="skill-icon" /></div></div>
+      {wellIcons.map(wellIcons => (
+        <div key={wellIcons} className='content-frame'>
+          <div className='content' dangerouslySetInnerHTML={{ __html: wellIcons }}></div>
+        </div>
       ))
       }
+      {/* {width > 500 && (
+        console.log(500)
+      )} */}
     </div>
+
   )
 }
 
