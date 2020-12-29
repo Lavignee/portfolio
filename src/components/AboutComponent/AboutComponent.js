ea8e30a6acdd99affcb3180ee900ce66566da2ec
Lavignee <ldy9243@gmail.com>
2020-12-30 04:04:59 +0900

    1. about 세션 디자인 일부 수정 2. about detail에 성장배경 디자인 3. contact의 애니메이션 작동방식 변경

--- src/components/AboutComponent/AboutComponent.js
+++ src/components/AboutComponent/AboutComponent.js
@@ -12,7 +12,12 @@ gsap.registerPlugin(ScrollTrigger);
 const AboutComponent = () => {
   const [nextText, setNextText] = useState(false);
   const savedChangeTarget = useRef();
-  useEffect(() => {
+
+  function changeText() {
+    setNextText(!nextText)
+  }
+
+  const aboutGsap = () => {
     gsap.fromTo('.about-title', {
       y: -100 + '%'
     }, {
@@ -26,11 +31,12 @@ const AboutComponent = () => {
         scrub: true
       }
     });
-  }, [])
-
-  function changeText() {
-    setNextText(!nextText)
   }
+
+  useEffect(() => {
+    aboutGsap();
+    return () => aboutGsap();
+  }, [])
 
   useEffect(() => {
     savedChangeTarget.current = changeText;
@@ -53,19 +59,19 @@ const AboutComponent = () => {
         </div>
 
         <div className='row'>
-          <div className='col-3 pr-none picture-frame'>
+          <div className='col-4 pl-pr-none picture-frame'>
             <div className='one'>
-              <img src={aboutOne} alt="creator picture" />
+              <img src={aboutOne} alt='creator picture' />
             </div>
           </div>
-          <div className='col-4 off-xs-1 pl-pr-none picture-frame'>
+          <div className='col-4 pl-pr-none picture-frame'>
             <div className='two'>
-              <img src={aboutTwo} alt="creator picture" />
+              <img src={aboutTwo} alt='creator picture' />
             </div>
           </div>
-          <div className='col-3 off-xs-1 pl-none picture-frame'>
+          <div className='col-4 pl-pr-none picture-frame'>
             <div className='three'>
-              <img src={aboutThree} alt="creator picture" />
+              <img src={aboutThree} alt='creator picture' />
             </div>
           </div>
         </div>
@@ -77,16 +83,7 @@ const AboutComponent = () => {
         </div>
 
         <div className='about-detail-button'>
-          <Link to='about'><button>About More<svg width="164" height="70" viewBox="0 0 164 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clipPath="url(#clip0)">
-              <path d="M103.4 69.1L115.6 69.1L118.4 69.1L131.8 69.1L163.2 69.1L156.7 62.6L94.1002 -3.02046e-06L84.9002 9.2L131.8 56.1L118.4 56.1L66.2002 3.9L64.8002 5.3L115.6 56.1L103.4 56.1L51.2002 3.9L49.8002 5.3L100.6 56.1L0.000195912 56.1L0.000195344 69.1L100.6 69.1L103.4 69.1Z" fill="#181818" />
-            </g>
-            <defs>
-              <clipPath id="clip0">
-                <rect width="69.1" height="163.2" fill="white" transform="translate(163.2) rotate(90)" />
-              </clipPath>
-            </defs>
-          </svg></button></Link>
+          <Link to='/about'><button>More Detail</button></Link>
         </div>
       </div>
     </section >
--- src/components/ContactComponent/ContactComponent.js
+++ src/components/ContactComponent/ContactComponent.js
@@ -1,9 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeContactState, changeContactDelay } from '../../Modules/CommonValueModule';
 import CustomInputComposition from '../../compositions/CustomInputComposition';
 import CustomTextareaComposition from '../../compositions/CustomTextareaComposition';
 import CustomButtonComposition from '../../compositions/CustomButtonComposition';
-import { useDispatch, useSelector } from 'react-redux';
-import { changeContactState, changeContactDelay } from '../../Modules/CommonValueModule';
+import SplitTextComponent from '../SplitTextComponent';
+import useInterval from "../../utils/useInterval";
+import kakaoIcon from '../../Static/images/kakao-icon.svg';
+import mobileIcon from '../../Static/images/mobile-icon.svg';
+import mailIcon from '../../Static/images/mail-icon.svg';
 import './ContactComponent.scss';
 
 const ContactComponent = () => {
@@ -16,13 +21,37 @@ const ContactComponent = () => {
   const { currentContactDelay } = useSelector(state => ({
     currentContactDelay: state.CommonValueModule.currentContactDelay
   }));
+  const [qnumber, setQnumber] = useState(0);
+  const qnumberRef = useRef(qnumber);
+  qnumberRef.current = qnumber;
 
-  useEffect(() => {
+  const contactButtonDelay = () => {
     onChangeContactDelay(true);
     setTimeout(() => {
       onChangeContactDelay(false);
     }, 1000)
+  }
+
+  const contactAnimation = () => {
+    setQnumber(1)
+    for (let i = 2; i < 4; i++) {
+      setTimeout(() => {
+        setQnumber(i)
+      }, 7000 * (i - 1));
+    }
+  }
+
+  useEffect(() => {
+    contactButtonDelay();
   }, [currentContactState]);
+
+  useEffect(() => {
+    contactAnimation();
+  }, []);
+
+  useInterval(() => {
+    contactAnimation();
+  }, 21000);
 
   return (
     <>
@@ -35,41 +64,59 @@ const ContactComponent = () => {
       </div>
 
       <div className={`contact-frame${currentContactState ? ' open' : ' close'}`}>
-        <div className='contact-grid-frame'>
-          <div className='contact-grid'></div>
-          <div className='contact-grid'></div>
-          <div className='contact-grid'></div>
-          <div className='contact-grid'></div>
-          <div className='contact-grid'></div>
-          <div className='contact-grid'></div>
-          <div className='contact-grid'></div>
-          <div className='contact-grid'></div>
-        </div>
+        {/* {currentContactState && ( */}
+        <>
+          <div className='contact-grid-frame'>
+            <div className='contact-grid'></div>
+            <div className='contact-grid'></div>
+            <div className='contact-grid'></div>
+            <div className='contact-grid'></div>
+            <div className='contact-grid'></div>
+            <div className='contact-grid'></div>
+            <div className='contact-grid'></div>
+            <div className='contact-grid'></div>
+          </div>
 
-        <div className='contact-content-frame'>
           <div className='container'>
-            <div className='row contact-content'>
-              <div className='col-12 col-s-6 col-l-4'>
-                <div className={`email-form-frame${currentContactState ? ' open' : ' close'}`}>
-                  <CustomInputComposition type={'text'} placeholder={'Name'} />
-                  <CustomInputComposition type={'text'} placeholder={'Phone'} />
-                  <CustomInputComposition type={'email'} placeholder={'Email'} />
-                  <CustomInputComposition type={'text'} placeholder={'Title'} />
-                  <CustomTextareaComposition placeholder={'content'} />
-                  <CustomButtonComposition text={'보내기'} align={'right'} />
-                </div>
-              </div>
+            <div className={`back-text${currentContactState ? ' open' : ' close'}`}>
+              {qnumberRef.current === 1 && (
+                <SplitTextComponent animation={'up'} scroll={'all'} depth>What should I do for you?</SplitTextComponent>
+              )}
+              {qnumberRef.current === 2 && (
+                <SplitTextComponent animation={'up'} scroll={'all'} depth>Could you tell me about the project?</SplitTextComponent>
+              )}
+              {qnumberRef.current === 3 && (
+                <SplitTextComponent animation={'up'} scroll={'all'} depth>I will reply by email as soon as possible.</SplitTextComponent>
+              )}
+            </div>
+          </div>
 
-              <div className='col-12 col-s-6 col-l-4'>
-                <div className={`info-frame${currentContactState ? ' open' : ' close'}`}>
-                  <span>phone: +82 010-2690-9243</span>
-                  <span>kakao: Lavignee</span>
-                  <span>email: doyoung9243@naver.com</span>
+          <div className='contact-content-frame'>
+            <div className='container'>
+              <div className='row contact-content'>
+                <div className='col-12 col-s-6 col-l-4'>
+                  <div className={`email-form-frame${currentContactState ? ' open' : ' close'}`}>
+                    <CustomInputComposition type={'text'} placeholder={'Name'} />
+                    <CustomInputComposition type={'text'} placeholder={'Phone'} />
+                    <CustomInputComposition type={'email'} placeholder={'Email'} />
+                    <CustomInputComposition type={'text'} placeholder={'Title'} />
+                    <CustomTextareaComposition placeholder={'Content'} />
+                    <CustomButtonComposition text={'보내기'} align={'right'} />
+                  </div>
+                </div>
+
+                <div className='col-12 col-s-6 col-l-4'>
+                  <div className={`info-frame${currentContactState ? ' open' : ' close'}`}>
+                    <div><span><img src={kakaoIcon} alt='kakao app icon' /></span> Lavignee</div>
+                    <div><span><img src={mobileIcon} alt='mobile icon' /></span> +82 010.2690.9243</div>
+                    <div><span><img src={mailIcon} alt='letter icon' /></span> doyoung9243@naver.com</div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </>
+        // )}
       </div>
     </>
   )
