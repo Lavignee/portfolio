33070fac9ebd2f2da0ab31329117e49aa7adbf63
Lavignee <ldy9243@gmail.com>
2020-12-30 03:29:12 +0900

    1. 이미지 일부 수정 및 메인 동영상 변경 2. 공용 css 일부 추가 및 사이트 기본 글꼴 변경 3. 편리성 위한 일부 스크립트 수정 및 추가

--- src/utils/ScrollToTop.js
+++ src/utils/ScrollToTop.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
-
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 function ScrollToTop({ history, children }) {
   const reactRoot = document.getElementById('root')
+
+  const unlisten = history.listen(() => {
+    history.location.hash === '' && reactRoot.scrollTo(0, 0);
+  });
+
   useEffect(() => {
-    const unlisten = history.listen(() => {
-      history.location.hash === '' && reactRoot.scrollTo(0, 0);
-    });
-    return () => {
-      unlisten();
-    }
+    ScrollTrigger.refresh();
+    return () => unlisten();
   }, []);
 
   return <>{children}</>;
--- src/utils/useWindowSize.js
+++ src/utils/useWindowSize.js
@@ -7,16 +7,13 @@ export default function useWindowSize() {
     height: isSSR ? window.innerHeight : 800,
   });
 
-  function changeWindowSize() {
+  const changeWindowSize = () => {
     setWindowSize({ width: window.innerWidth, height: window.innerHeight });
   }
 
   useEffect(() => {
     window.addEventListener("resize", changeWindowSize);
-
-    return () => {
-      window.removeEventListener("resize", changeWindowSize);
-    };
+    return () => window.removeEventListener("resize", changeWindowSize);
   }, []);
 
   return windowSize;
